420220a171207cfe5b05008bd24b1048af1a9072
James Montemagno <james.montemagno@gmail.com>
2019-07-20 23:34:41 -0700

    Initial go at "stateful" item

---
+++ MvvmHelpers.Samples/MvvmHelpers.Samples/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MvvmHelpers.Samples
+{
+    public class MainViewModel
+    {
+        public Stateful<string> Name { get; } = new Stateful<string>();
+        public Stateful<int> Count { get; } = new Stateful<int>();
+        public Stateful<string> CountLabel { get; } = new Stateful<string>("Click Me!");
+        public Command IncreaseCountCommand { get; }
+
+        public MainViewModel()
+        {
+            IncreaseCountCommand = new Command(ExecuteIncreaseCommand);
+        }
+
+        private void ExecuteIncreaseCommand()
+        {
+            Count.Value++;
+            CountLabel.Value = $"You clicked {Count.Value} times.";
+        }
+    }
+}
---
+++ MvvmHelpers/Stateful.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmHelpers
+{
+    public class Stateful<T> : ObservableObject
+    {
+        public Stateful() =>
+            backing = default;
+
+        public Stateful(T defaultValue) =>
+            backing = defaultValue;
+
+        T backing;
+        public T Value
+        {
+            get => backing;
+            set => SetProperty(ref backing, value);
+        }
+    }
+
+    public class MyViewModel
+    {
+        public Stateful<int> Count { get; } = new Stateful<int>(1);
+
+        public void CountUp()
+        {
+            var current = Count.Value;
+
+            Count.Value = current + 1;
+        }
+    }
+}
